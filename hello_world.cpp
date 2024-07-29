b781470e1d88454b1fcf3d42be4cf9fb26660b34
Sam Osman <147906446+samosman001@users.noreply.github.com>
2024-07-29 00:01:36 -0500

    Created "Hello, World!" Source Code

---
+++ hello_world.cpp
@@ -0,0 +1,21 @@
+// hello_world.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Created by Samatar Osman
+// 07/28/2024
+
+#include <iostream>
+int main() // C++ programs start by executing the function main
+{
+    std::cout << "Hello, World!\n\n"; // output "Hello World!"
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started:
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
